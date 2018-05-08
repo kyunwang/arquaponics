68bd9a2790589d39026f317d2cb9330c637bf838
VincentKempers <me@vncn.tk>
2018-05-08 17:48:07 +0200

    add the url

--- server/routes/data-api.js
+++ server/routes/data-api.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const arController = require('../controllers/arController');
+const Stomp = require('@stomp/stompjs');
 
 const router = express.Router();
+const client = Stomp.client(process.env.THE_URL);
 
 router.get('/', arController.apiTest);
 
