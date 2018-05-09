2f29b506ce93874df123bfde08cb9b978042c702
VincentKempers <me@vncn.tk>
2018-05-09 11:26:52 +0200

    establish connection, not ready

--- server/routes/data-api.js
+++ server/routes/data-api.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const arController = require('../controllers/arController');
+const sockjs = require('sockjs');
 // const Stomp = require('@stomp/stompjs');
 
 const router = express.Router();
 // const client = Stomp.client(process.env.THE_URL);
 // const allPower = '/exchange/power/#';
 
+const echo = sockjs.createServer({ sockjs_url: 'http://cdn.jsdelivr.net/sockjs/1.0.1/sockjs.min.js'});
+echo.on('connection', function(connec) {
+  connec.on('data', function(message){
+    conn.write(message);
+    console.log(message);
+  });
+  conn.on('close', function() {
+    console.log('closed');
+  });
+});
 
 router.get('/', arController.apiTest);
+
 console.log(arController.apiTest);
 
 module.exports = router;
