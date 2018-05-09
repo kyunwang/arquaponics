a3e37bc8415fffe74e50214b0dd1e414c0aad9e8
VincentKempers <me@vncn.tk>
2018-05-09 14:02:51 +0200

    got a connection going

--- server/routes/data-api.js
+++ server/routes/data-api.js
@@ -1,25 +1,41 @@
 const express = require('express');
 const arController = require('../controllers/arController');
 const sockjs = require('sockjs');
-// const Stomp = require('@stomp/stompjs');
+const Stomp = require('@stomp/stompjs');
 
 const router = express.Router();
-// const client = Stomp.client(process.env.THE_URL);
-// const allPower = '/exchange/power/#';
+const client = Stomp.client('ws://stomp.spectral.energy:15674/ws');
+const allPower = '/exchange/power/#';
 
-const echo = sockjs.createServer({ sockjs_url: 'http://cdn.jsdelivr.net/sockjs/1.0.1/sockjs.min.js'});
-echo.on('connection', function(connec) {
-  connec.on('data', function(message){
-    conn.write(message);
-    console.log(message);
-  });
-  conn.on('close', function() {
-    console.log('closed');
-  });
-});
+require('dotenv').config({path:'./vars.env'});
+
+client.connect(process.env.USERNAME, 'mnwdTGgQu5zPmSrz', onConnect, console.error, '/');
+// client.connect('web', 'mnwdTGgQu5zPmSrz', onConnect, console.error, '/');
+
+
+function onConnect(someStuff) {
+  // console.log('data', someStuff);
+  console.log(allPower, someStuff);
+  client.subscribe(allPower, onData(someStuff));
+};
+
+function onData(data) {
+  console.log(data.body);
+  // data = JSON.parse(data.body);
+};
+
+// const echo = sockjs.createServer({ sockjs_url: 'http://cdn.jsdelivr.net/sockjs/1.0.1/sockjs.min.js'});
+// echo.on('connection', function(connec) {
+//   connec.on('data', function(message){
+//     connec.write(message);
+//     console.log(message);
+//   });
+//   connec.on('close', function() {
+//     console.log('closed');
+//   });
+// });
 
 router.get('/', arController.apiTest);
 
-console.log(arController.apiTest);
 
 module.exports = router;
