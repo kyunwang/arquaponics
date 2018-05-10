8a5e130bf20dbfdadd6b57ccf9f036259787993f
Kangyun Wang <wky@kangyuns-mbp.home>
2018-05-10 14:15:08 +0200

    Data now updateable in line graph (block position only)

--- public/scripts/arGraphs.js
+++ public/scripts/arGraphs.js
@@ -23,13 +23,12 @@ var fakeData = [
 	200,
 ];
 
-function render(data) {
-	// we scale the height of our bars using d3's linear scale
-	var hscale = d3
-		.scaleLinear()
-		.domain([0, d3.max(data)])
-		.range([0, 3]);
+// we scale the height of our bars using d3's linear scale
+var hscale = d3.scaleLinear().range([0, 3]);
 
+var greenhouseLine = d3.select('#a-greenhouse-line');
+
+function render(data) {
 	// we select the scene object just like an svg
 	var scene = d3.select('#a-greenhouse');
 
@@ -69,22 +68,19 @@ function render(data) {
 }
 
 function renderLine(data) {
-	var hscale = d3
-		.scaleLinear()
-		.domain([0, d3.max(data)])
-		.range([0, 3]);
+	console.log('renderline', data);
+	hscale.domain([0, d3.max(data)]);
 
 	// we select the scene object just like an svg
-	var curveTrack = d3.select('#track1');
+	var curveTrack = greenhouseLine.selectAll('a-curve-point').data(data);
 
 	// we use d3's enter/update/exit pattern to draw and bind our dom elements
 	// var bars = scene.selectAll('a-cube.bar').data(data);
 
 	curveTrack
-		.selectAll('a-curve-point')
-		.data(data)
 		.enter()
 		.append('a-curve-point')
+		.merge(curveTrack)
 		.attr('position', function(d, i) {
 			var position = {
 				x: 0.15 * (i * 2),
@@ -95,9 +91,50 @@ function renderLine(data) {
 				z: 0 - hscale(d) / 2,
 			};
 			return position;
+		})
+		.attr('geometry', function(d) {
+			var attr = {
+				primitive: 'box',
+				height: 0.1,
+				width: 0.1,
+				depth: 0.1,
+			};
+
+			return attr;
 		});
+	// .attr('material', 'material="color:red;"');
+
 	// .attr('depth', 0.2);
+
+	// EXIT
+	// Remove old elements as needed.
+	curveTrack.exit().remove();
+
+	// paintLine();
 }
 
+function paintLine() {
+	// greenhouseLine.insert('a-text');
+	var ghl = document.getElementById('a-greenhouse-line');
+	console.log('painting', ghl);
+
+	ghl.insertAdjacentHTML(
+		'afterend',
+		'<a-draw-curve curveref="#a-greenhouse-line" material="shader: line; color: blue;"></a-draw-curve>'
+	);
+}
+
+// Initial render
 // render(fakeData);
 renderLine(fakeData);
+
+// Grab a random sample of letters from the alphabet, in alphabetical order.
+d3.interval(function() {
+	var shuffle = d3.shuffle(fakeData);
+	// console.log('interval', shuffle);
+
+	renderLine(shuffle);
+	// update(d3.shuffle(alphabet)
+	// 	 .slice(0, Math.floor(Math.random() * 26))
+	// 	 .sort());
+}, 1500);
