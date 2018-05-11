5e2fc03e106f5d4a3bd1551f002c5f73c62b3dfd
nourbayard <nour@matise.nl>
2018-05-11 12:21:15 +0200

    initial zerostate commit
    Added zeroState page + css
    Updated title in header
    Added zeroState in router
    Added zeroState AR marker

--- server/controllers/websiteController/index.js
+++ server/controllers/websiteController/index.js
@@ -1,3 +1,7 @@
 exports.homePage = (req, res) => {
 	res.render('website/homePage', { message: 'Homepage website' });
 };
+
+exports.zeroState = (req, res) => {
+	res.render('website/zeroState', { message: 'Zero state' });
+};
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,5 +4,6 @@ const wController = require('../controllers/websiteController');
 const router = express.Router();
 
 router.get('/', wController.homePage);
+router.get('/zeroState', wController.zeroState);
 
 module.exports = router;
